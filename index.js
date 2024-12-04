3226d141bc905c56fbb943b419deb392c408d47b
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-12-04 22:32:19 +0100

    some listeners

--- index.js
+++ index.js
@@ -3,15 +3,32 @@
 
     let $window = null;
 
+    const isMac = process.platform === 'darwin';
+    const isClosed = BrowserWindow.getAllWindows().length === 0;
+    const isDevelopment = process.env.NODE_ENV == 'development';
+
     function createWindow () {
         $window = new BrowserWindow({
             width: 500,
             height: 600,
-            icon: `${__dirname}/src/icons/Icon_256x256.png`
+            icon: `${__dirname}/src/icons/Icon_256x256.png`,
         });
 
         $window.loadFile('./src/index.html');
     }
 
+    function closeWindows () {
+        if(!isMac) app.quit();
+    }
+
+    function restart () {
+        if(isClosed) {
+            createWindow();
+        }
+    }
+
     app.on('ready', createWindow);
+    app.on('window-all-closed', closeWindows);
+    app.on('activate', restart);
+    app.allowRendererProcessReuse = true;
 })();

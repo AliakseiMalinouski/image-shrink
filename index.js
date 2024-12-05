72d50cec852b25a765ce614a9bdd46ea4f6a16e2
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-12-05 19:24:01 +0100

    added menu

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 (function () {
-    const { app, BrowserWindow } = require('electron');
+    const { app, BrowserWindow, Menu } = require('electron');
 
     let $window = null;
+    let $mainMenu = null;
 
     const isMac = process.platform === 'darwin';
     const isClosed = BrowserWindow.getAllWindows().length === 0;
     const isDevelopment = process.env.NODE_ENV == 'development';
+
+    const menu = [
+        ...(isMac && [{ role: 'appMenu' }]),
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Quit',
+                    click: () => app.quit(),
+                }
+            ],
+        }
+    ];
 
     function createWindow () {
         $window = new BrowserWindow({
@@ -15,6 +29,12 @@
         });
 
         $window.loadFile('./src/index.html');
+        $window.on('ready', () => $window = null);
+    }
+
+    function createMenu () {
+        $mainMenu = Menu.buildFromTemplate(menu);
+        Menu.setApplicationMenu($mainMenu);
     }
 
     function closeWindows () {
@@ -22,12 +42,12 @@
     }
 
     function restart () {
-        if(isClosed) {
-            createWindow();
-        }
+        if(isClosed) return;
+        createWindow();
     }
 
     app.on('ready', createWindow);
+    app.on('ready', createMenu);
     app.on('window-all-closed', closeWindows);
     app.on('activate', restart);
     app.allowRendererProcessReuse = true;
